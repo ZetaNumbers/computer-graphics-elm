291be8029d929d00407f12767bc89d8d06c0131d
zetanumbers <zeta0.114942@gmail.com>
2021-03-30 23:38:28 +0300

    Updates

---
+++ precache-manifest.869967582aa7ffcf44e3fb454d258264.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60a876d56af951f1dc966f408d1202b",
+    "url": "/computer-graphics-elm/Cone.obj"
+  },
+  {
+    "revision": "8d0fd75372f9c6ce888e5935b9eb87b4",
+    "url": "/computer-graphics-elm/Cube.obj"
+  },
+  {
+    "revision": "c1e0416c76fee0bb051f85c69a8fe7c0",
+    "url": "/computer-graphics-elm/Cylinder.obj"
+  },
+  {
+    "revision": "f689596dbf01468557daca40833afa98",
+    "url": "/computer-graphics-elm/Snowman.blend"
+  },
+  {
+    "revision": "cebb440f2a8d996334591735812f2064",
+    "url": "/computer-graphics-elm/Snowman.obj"
+  },
+  {
+    "revision": "087f5062ba06d0f8c681bc4f6abd6707",
+    "url": "/computer-graphics-elm/Sphere.obj"
+  },
+  {
+    "revision": "59406c94d118015fecf47d2d6750af41",
+    "url": "/computer-graphics-elm/Torus.obj"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/computer-graphics-elm/favicon.ico"
+  },
+  {
+    "revision": "57774736d0cde2645b58b8360d10c60d",
+    "url": "/computer-graphics-elm/index.html"
+  },
+  {
+    "revision": "2c66e99bce7bddf36d9cd239ebd7eb83",
+    "url": "/computer-graphics-elm/manifest.json"
+  },
+  {
+    "revision": "392357ce47fc753c61b2",
+    "url": "/computer-graphics-elm/static/css/main.89763d65.chunk.css"
+  },
+  {
+    "revision": "392357ce47fc753c61b2",
+    "url": "/computer-graphics-elm/static/js/main.392357ce.chunk.js"
+  },
+  {
+    "revision": "4eec1a21ac15f8a672e3",
+    "url": "/computer-graphics-elm/static/js/runtime~main.4eec1a21.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/computer-graphics-elm/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
